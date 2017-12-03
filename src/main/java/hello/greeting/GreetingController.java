6edb3a4123d78b00ae9680da43a668b3ce3824b7
Maarten Raes <maarten.raes@student.odisee.be>
2017-12-03 16:31:45 +0100

    hotfix

--- src/main/java/hello/greeting/GreetingController.java
+++ src/main/java/hello/greeting/GreetingController.java
@@ -12,13 +12,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value="/greeting", method= RequestMethod.GET)
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-    @RequestMapping(value="/greeting", method= RequestMethod.GET)
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
