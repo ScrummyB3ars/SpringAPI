c8803aec9be58512dc18d08ce635056b6c090031
Maarten Raes <maarten.raes@student.odisee.be>
2017-12-08 20:59:29 +0100

    test update

--- src/test/java/hello/GreetingControllerTests.java
+++ src/test/java/hello/GreetingControllerTests.java
@@ -33,17 +33,16 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class GreetingControllerTests {
 
-    /*@Autowired
+    @Autowired
     private MockMvc mockMvc;
 
-    /*@Test
-    /*public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, World!"));
+        this.mockMvc.perform(get("/sub")).andDo(print()).andExpect(status().isOk());
     }
 
-    @Test
+    /*@Test
     public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
         this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
